e70f88fadd8b552cb5a8d10b5aeac4078829b05a
Daria <daria.kaygo@yandex.ru>
2019-09-04 20:35:44 +0500

    init commit

---
+++ src/main/java/com/darakay/marcc/TranslatorMarccApplication.java
@@ -0,0 +1,13 @@
+package com.darakay.marcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorMarccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatorMarccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darakay/marcc/translatormarcc/TranslatorMarccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.darakay.marcc.translatormarcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TranslatorMarccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
